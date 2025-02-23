bf488436b47a16ee8fbb25716191f85f80c5d8eb
EyalShabo <eyal.shabo.mail@gmail.com>
2025-02-23 11:32:00 +0200

    load app

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 from models import db
 import os
+<<<<<<< Updated upstream
 from config import Config
 from models.admin import Admin
 from models.foodType import FoodType
@@ -11,13 +12,24 @@ from models.mealsInOrder import MealsInOrder
 from models.order import Order
 from models.table import Table
 
+=======
+from models.user import db, User
+from config import Config
+>>>>>>> Stashed changes
 
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.config.from_object(Config)
+<<<<<<< Updated upstream
+db.init_app(app)
+=======
+>>>>>>> Stashed changes
+
 db.init_app(app)
 
+with app.app_context():
+    db.create_all()
 
 @app.route('/test', methods=['GET'])
 def get_books():
