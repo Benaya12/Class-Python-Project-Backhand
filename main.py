a1024c1d78abbe32716dd5f0d35478f1b24ce366
danielcm2802 <155140815+danielcm2802@users.noreply.github.com>
2025-02-23 11:21:35 +0200

    ss

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ from models.table import Table
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from models import db
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+username = os.getenv('DB_NAME')
+password = os.getenv('DB_PASSWORD')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:dani2802@localhost/restaurant'  
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{username}:{password}@localhost/restaurant'  
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
